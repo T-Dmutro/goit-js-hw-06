ecc37f6083cc112dea793f58b86fd917d6f723ac
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-02-01 20:44:53 +0200

    Initial commit

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const liItem = document.querySelectorAll("li.item");
+// console.log("Number of categories:" ,liItem.length);
+// console.log(navEl)
+const LiCategory =  document.querySelectorAll("h2");
+// console.log(LiCategory)
+
