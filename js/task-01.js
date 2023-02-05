6069b4981075f720403a4d2095f4b879f553a8bd
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-02-05 14:06:16 +0200

    task 2

--- js/task-01.js
+++ js/task-01.js
@@ -2,5 +2,27 @@ const liItem = document.querySelectorAll("li.item");
 // console.log("Number of categories:" ,liItem.length);
 // console.log(navEl)
 const LiCategory =  document.querySelectorAll("h2");
-// console.log(LiCategory)
+const ulCategory =document.querySelectorAll("ul")
+// console.log('Category:', LiCategory[0].textContent)
 
+// console.log('Category:', LiCategory[1].textContent)
+// console.log('Category:', LiCategory[2].textContent)
+
+
+
+
+
+
+
+
+
+
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,31 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients")
+
+
+const listFull = ingredients.map(option =>{
+    const el = document.createElement("li")
+  el.textContent=option;
+  el.classList.add('item')
+  
+  
+  // console.log(el)
+  return el
+});
+console.log(list)
+
+
+
+
+// for (const ingredient of ingredients){
+//   console.log(ingredient)
+//   const el = document.createElement("li")
+//   el.textContent=ingredient;
+//   el.classList.add('item')
+//   // list.after(el)
+//   listFull.push(el)
+//   console.log(listFull)
+// };
+list.append(...listFull)
