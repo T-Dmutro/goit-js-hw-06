510fa5fca8e01cf84f5485f7b715a336644a568b
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-02-06 20:14:31 +0200

    task8 part 1

--- js/task-08.js
+++ js/task-08.js
@@ -3,14 +3,22 @@ const form = document.querySelector('.login-form')
 
 form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit (event){
-   
-    
-    const formEl = event.currentTarget.
-    console.dir(formEl);
-    const mail = formEl.email;
-    const password = formEl.password;
 
+function onFormSubmit (event){
     event.preventDefault();
     console.log(`ура працює`)
+    
+    const formEl = document.forms[0].elements;
+    console.dir(formEl);
+    const mail = formEl.email.value;
+    const password = formEl.password.value;
+    console.log(mail, password)
+    const submitValue = {mail: this.mail, password: this.password};
+    console.log( submitValue);
+   
 }
+// Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.---?
+// Під час відправлення форми сторінка не повинна перезавантажуватися.---зроблено
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
