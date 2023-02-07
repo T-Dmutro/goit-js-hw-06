20dac317ee0f33840cebaa86bddbeae4e0d3aa1c
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-02-07 20:09:44 +0200

    task 8 full

--- js/task-08.js
+++ js/task-08.js
@@ -7,30 +7,32 @@ form.addEventListener('submit', onFormSubmit);
 function onFormSubmit (event){
     event.preventDefault();
     // console.log(`ура працює`)
-    
+    function resetForm() {
+        form.reset();
+      };
     const formEl = document.forms[0].elements;
     // console.dir(formEl);
     const mail = formEl.email.value;
     const password = formEl.password.value;
     // console.log(mail, password)
     const submitValueEl = {
-        mail: mail,
-        password: password,
+        mail: this.mail,
+        password: this.password,
        };
    if( mail=== ""|| password === ""){
     alert("Всі поля мають бути заповені")
-   }
+   }else{
+    resetForm();
     submitValueEl.mail = mail;
     submitValueEl.password = password;
+    console.log( submitValueEl);
+   }
+        
  
-   
-   
-   console.log( submitValueEl);
-
    
 }
 // Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.---?
 // Під час відправлення форми сторінка не повинна перезавантажуватися.---зроблено
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені. ---зроблено
-// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
-// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.---зроблено
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.--- зроблено
