38ff77aca99db5f81e23f60331dec45c9dc0ae16
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-02-07 19:53:03 +0200

    task 9 full

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const button =document.querySelector("button");
+const span = document.querySelector(".color");
+console.log(span);
+console.log(button);
+button.addEventListener('click', onChangeColor);
+function onChangeColor(){
+  document.body.style.backgroundColor= getRandomHexColor();
+  span.textContent= getRandomHexColor();
+};
+
